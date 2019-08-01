3a221e3bd2885572aeada42a82a35c2b27fbd00c
HueWoon Kim <kho0307@gmail.com>
2019-08-01 16:53:58 +0900

    Initial Commit

---
+++ syncAndAsync/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  syncAndAsync
+//
+//  Created by Hue Woon Kim on 01/08/2019.
+//  Copyright © 2019 Hue Woon Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
